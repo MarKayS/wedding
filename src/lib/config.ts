a47d1cb888c50b472b6f2a26e06781fe1ff60839
marekkajfosz <marekkajfosz@gmail.com>
2025-05-03 21:11:46 +0200

    start

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,10 +4,3 @@ export const siteConfig = {
   description: "This is a fantastic Astro site",
   githubRepo: "aholbreich/astrostart_TDA"
 };
-
-// Feature flags examles
-export const featureFlags = {
-  enableBetaFeatures: false,
-  enableImprint: true,
-  enableDataprotection: false,
-};
